c6642ba9c6b38547a38d82c3c528c872c8ee540b
Lewis Hadden <lewishadden@gmail.com>
2026-02-21 18:13:00 +0000

    remove admin link from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,17 +36,6 @@ export default function Footer() {
               </li>
             </ul>
           </div>
-
-          <div>
-            <h3 className="font-semibold text-foam mb-4">Admin</h3>
-            <ul className="space-y-2 text-sm">
-              <li>
-                <Link href="/admin/login" className="hover:text-caramel transition-colors">
-                  Admin Login
-                </Link>
-              </li>
-            </ul>
-          </div>
         </div>
 
         <div className="border-t border-espresso-800 mt-8 pt-8 text-center text-xs text-espresso-500">
